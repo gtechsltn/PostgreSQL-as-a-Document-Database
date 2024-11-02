994cc0f601555e26b72d685d9cd83733ac9249cd
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-11-02 19:40:21 +0600

    hasan

---
+++ PostgresMartenApi/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Marten;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostgresMartenApi.Models;
+
+namespace PostgresMartenApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDocumentSession _session;
+
+        public ProductsController(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            var products = await _session.Query<Product>().ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Post([FromBody] Product product)
+        {
+            _session.Store(product);
+            _session.SaveChanges();
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+    }
+}
---
+++ PostgresMartenApi/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using Marten.Schema;
+
+namespace PostgresMartenApi.Models
+{
+    public class Product
+    {
+        [Identity] // This will tell Marten to use this property as the identifier
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
